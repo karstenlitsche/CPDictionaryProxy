aaf81e155b443e685bfa49ec61f873bd8a08215e
Karsten Litsche <k.litsche@gmx.de>
2012-07-15 18:06:27 +0200

    add class and example plist

---
+++ CPDictionaryProxy/CPDictionaryProxy.h
@@ -0,0 +1,9 @@
+
+//  Created by Karsten Litsche on 15.07.12.
+//  Copyright (c) 2012 Compeople. All rights reserved.
+
+#import <Foundation/Foundation.h>
+
+@interface CPDictionaryProxy : NSProxy
+
+@end
---
+++ CPDictionaryProxy/CPDictionaryProxy.m
@@ -0,0 +1,9 @@
+
+//  Created by Karsten Litsche on 15.07.12.
+//  Copyright (c) 2012 Compeople. All rights reserved.
+
+#import "CPDictionaryProxy.h"
+
+@implementation CPDictionaryProxy
+
+@end
